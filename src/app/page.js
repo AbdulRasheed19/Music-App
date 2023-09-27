88188e3b9d5ccba5212f7f0bbaaa16ae372674cc
Raja Naveed <61252048+raja-naveed@users.noreply.github.com>
2023-09-27 10:05:30 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Herosection from "./components/Herosection";
 export default function Home() {
 
   return (
-    <>
+    <div>
       <Herosection title={"LET\'S  WATCH MOVIE TOGETHER"} imageUrl={'/home.svg'}/>
-    </>
+    </div>
   )
 }
